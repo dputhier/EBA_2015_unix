4868745e11f555ea9317b8b328eb5590e2879b4f
puthier <puthier@gmail.com>
2015-07-08 12:08:50 +0200

    Saving...

--- source/conf.py
+++ source/conf.py
@@ -117,9 +117,24 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'alabastera'
 html_theme = 'nature'
 
+# import alabaster
+
+# html_theme_path = [alabaster.get_path()]
+# extensions = ['alabaster']
+# html_theme = 'alabaster'
+# html_sidebars = {
+#     '**': [
+#         'about.html',
+#         'navigation.html',
+#         'relations.html',
+#         'searchbox.html',
+#         'donate.html',
+#     ]
+# }
+
+#html_theme='sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -180,7 +195,7 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
